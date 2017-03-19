afa492bfc032bb4e89c9d435bd52a3065d5ec4fc
Cloud Hong <Weiyun.Hong@lic.co.nz>
2017-03-20 11:55:25 +1300

    master - added a nuspec file

--- Library.Testing.EntityFramework/Properties/AssemblyInfo.cs
+++ Library.Testing.EntityFramework/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Library.Testing.EntityFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Testing extensions for EF (FakeDbSet)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Livestock Improvement")]
+[assembly: AssemblyCompany("Cloud Hong")]
 [assembly: AssemblyProduct("Library.Testing.EntityFramework")]
 [assembly: AssemblyCopyright("Copyright © Livestock Improvement 2017")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0-Pre")]
