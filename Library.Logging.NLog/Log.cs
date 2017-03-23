242e782f97ab5bc21c5bda6b1a11a15ad969f510
Cloud Hong <Weiyun.Hong@lic.co.nz>
2017-03-23 15:37:28 +1300

    develop - Add logging.nlog library

---
+++ Library.Logging.NLog/Log.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Library.Logging.v0;
+using NLog;
+
+namespace Library.Logging.NLog
+{
+	public class Log : ILog
+	{
+		private readonly Logger _logger;
+
+		public Log(Logger logger)
+		{
+			_logger = logger;
+		}
+
+		public void Debug(Func<string> logEntry)
+		{
+			if (_logger.IsDebugEnabled)
+				_logger.Debug(logEntry.Invoke());
+		}
+
+		public void Debug(Func<string> logEntry, Exception e)
+		{
+			if (_logger.IsDebugEnabled)
+				_logger.Debug(e, logEntry.Invoke());
+		}
+
+		public void Error(string logEntry)
+		{
+			if (_logger.IsErrorEnabled)
+				_logger.Error(logEntry);
+		}
+
+		public void Error(string logEntry, Exception e)
+		{
+			if (_logger.IsErrorEnabled)
+				_logger.Error(e, logEntry);
+		}
+
+		public void Fatal(string logEntry)
+		{
+			if (_logger.IsFatalEnabled)
+				_logger.Fatal(logEntry);
+		}
+
+		public void Fatal(string logEntry, Exception e)
+		{
+			if (_logger.IsFatalEnabled)
+				_logger.Fatal(e, logEntry);
+		}
+
+		public void Info(Func<string> logEntry)
+		{
+			if (_logger.IsInfoEnabled)
+				_logger.Info(logEntry.Invoke());
+		}
+
+		public void Info(string logEntry)
+		{
+			if (_logger.IsInfoEnabled)
+				_logger.Info(logEntry);
+		}
+
+		public void Info(string logEntry, Exception e)
+		{
+			if (_logger.IsInfoEnabled)
+				_logger.Info(e, logEntry);
+		}
+
+		public void Trace(Func<string> logEntry)
+		{
+			if (_logger.IsTraceEnabled)
+				_logger.Trace(logEntry.Invoke());
+		}
+
+		public void Trace(Func<string> logEntry, Exception e)
+		{
+			if (_logger.IsTraceEnabled)
+				_logger.Trace(e, logEntry.Invoke());
+		}
+
+		public void Warn(string logEntry)
+		{
+			if (_logger.IsWarnEnabled)
+				_logger.Warn(logEntry);
+		}
+
+		public void Warn(string logEntry, Exception e)
+		{
+			if (_logger.IsWarnEnabled)
+				_logger.Warn(e, logEntry);
+		}
+	}
+}
---
+++ Library.Logging.NLog/LogFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Library.Logging.v0;
+using NLog;
+
+namespace Library.Logging.NLog
+{
+	public class LogFactory : ILogFactory
+	{
+		public ILog CreateLog(Type type)
+		{
+			return new Log(LogManager.GetLogger(type.FullName));
+		}
+
+		public ILog CreateNamedLog(string name)
+		{
+			return new Log(LogManager.GetLogger(name));
+		}
+	}
+}
