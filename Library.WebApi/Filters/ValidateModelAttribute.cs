9b74912c261ae2ce93c95db52342d586e102864e
Cloud Hong <Weiyun.Hong@lic.co.nz>
2017-09-11 13:28:53 +1200

    升级webapi helper

---
+++ Library.WebApi/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Library.WebApi.Filters
+{
+	/// <summary>
+	/// Checks ModelState and returns a Bad Request response (400) if invalid
+	/// This means that the controller action will not be called.
+	/// </summary>
+	public class ValidateModelAttribute : ActionFilterAttribute
+	{
+		/// <inheritdoc />
+		public override void OnActionExecuting(HttpActionContext actionContext)
+		{
+			var modelState = actionContext.ModelState;
+
+			if (!modelState.IsValid)
+			{
+				actionContext.Response = actionContext.Request
+					.CreateErrorResponse(HttpStatusCode.BadRequest, modelState);
+			}
+		}
+	}
+}
